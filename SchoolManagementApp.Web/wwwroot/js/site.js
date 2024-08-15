4dfc0147ed1518f11057dbd84323fae7713cd6e6
BrVenturaU <brandonventura16@gmail.com>
2024-08-15 14:41:33 -0600

    Add deletion confirmation message.

--- SchoolManagementApp.Web/wwwroot/js/site.js
+++ SchoolManagementApp.Web/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const deleteForm = document.querySelectorAll('.delete');
+
+for (const formEl of deleteForm) {
+    formEl.onsubmit = (e) => {
+        const result = confirm("¿Desea eliminar el registro?")
+        if (!result) {
+            e.preventDefault();
+            return false;
+        }
+    }
+}
